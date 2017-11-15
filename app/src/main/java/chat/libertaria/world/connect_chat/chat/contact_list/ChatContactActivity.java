dd462a34eac450b6cee31397c0192612d83c8507
Yayotron <marsvicam@gmail.com>
2017-11-15 22:02:40 +0100

    Multiple changes: - Simplified the binding client service logic. - Fixed the message queue broadcast receiver - now we have only one global receiver. - Fixed minor bugs on chat.

--- app/src/main/java/chat/libertaria/world/connect_chat/chat/contact_list/ChatContactActivity.java
+++ app/src/main/java/chat/libertaria/world/connect_chat/chat/contact_list/ChatContactActivity.java
@@ -16,7 +16,6 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,10 +33,14 @@ public class ChatContactActivity extends BaseActivity {
     private static final int OPTION_ADD_CONTACT = 0;
     private static final int OPTION_SELECTED_PROFILE = 1;
     private static final int OPTION_CHANGE_PROFILE = 2;
+    private static final int DEFAULT_GROUP = 0;
 
 
     private View root;
     private ViewPager viewPager;
+
+    private boolean showMenu;
+
     @Override
     protected void onCreateView(Bundle savedInstanceState, ViewGroup container) {
         root = getLayoutInflater().inflate(R.layout.chat_contacts_activity, container);
@@ -91,28 +94,25 @@ public class ChatContactActivity extends BaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        MenuItem menuAdd = menu.add(OPTION_ADD_CONTACT, 0, Menu.NONE, R.string.add_contact).setIcon(R.drawable.ic_add_acontact);
+        MenuItem menuAdd = menu.add(DEFAULT_GROUP, OPTION_ADD_CONTACT, Menu.NONE, R.string.add_contact).setIcon(R.drawable.ic_add_acontact);
         menuAdd.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
-        MenuItem menuMyProfile = menu.add(OPTION_SELECTED_PROFILE, 1, Menu.NONE, R.string.my_profile);
+        MenuItem menuMyProfile = menu.add(DEFAULT_GROUP, OPTION_SELECTED_PROFILE, Menu.NONE, R.string.my_profile);
         menuMyProfile.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-        MenuItem menuOptions = menu.add(OPTION_CHANGE_PROFILE, 2, Menu.NONE, R.string.change_profile);
+        MenuItem menuOptions = menu.add(DEFAULT_GROUP, OPTION_CHANGE_PROFILE, Menu.NONE, R.string.change_profile);
         menuOptions.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
-
-
         return super.onCreateOptionsMenu(menu);
     }
 
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
             case OPTION_ADD_CONTACT:
                 // Add contacts
                 OpenConnectUtil.openSendRequestScreen(this);
                 return true;
             case OPTION_SELECTED_PROFILE:
-                OpenConnectUtil.openMyProfileScreen(this,selectedProfPubKey);
+                OpenConnectUtil.openMyProfileScreen(this, selectedProfPubKey);
                 break;
             case OPTION_CHANGE_PROFILE:
                 // Open settings
